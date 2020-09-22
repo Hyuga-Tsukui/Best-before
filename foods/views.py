21802a17300e3935e4a5fa8db894957d8cd657e9
Hyuga-Tsukui <gakikiki407@gmail.com>
2020-09-22 20:03:16 +0900

    Fix 賞味期限切れ画面の抽出条件を修正

--- foods/views.py
+++ foods/views.py
@@ -8,21 +8,22 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Food
 from .forms import FoodForm
-# Create your views here.
 
 
 class IndexView(LoginRequiredMixin, generic.ListView):
     template_name = "foods/index.html"
 
     def get_queryset(self):
-        return  Food.objects.filter(user=self.request.user).order_by('best_before')
+        return Food.objects.filter(user=self.request.user).order_by('best_before')
+
 
 class DeadLineListView(LoginRequiredMixin, generic.ListView):
     template_name = "foods/dead_line_list.html"
 
     def get_queryset(self):
         dead_line = timezone.now() + datetime.timedelta(days=3)
-        return  Food.objects.filter(user=self.request.user).filter(best_before__lt=dead_line.date()).order_by('best_before')
+        return Food.objects.filter(user=self.request.user).filter(best_before__lte=dead_line.date()).order_by('best_before')
+
 
 class CreateFoodView(LoginRequiredMixin, generic.CreateView):
     template_name = "foods/create_food.html"
@@ -33,6 +34,7 @@ class CreateFoodView(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.user_id = self.request.user.id
         return super(CreateFoodView, self).form_valid(form)
+
 
 @login_required
 def delete_food(request, food_id):
