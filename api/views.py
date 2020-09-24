b43b894f089d020882e3cef44cf864d7722c799c
Hyuga-Tsukui <gakikiki407@gmail.com>
2020-09-24 17:38:13 +0900

    Change 画面に表示する際にvueを利用

--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import permissions
 
-from .serializers.food_serializers import FoodSerializer
 from foods.models import Food
+from .serializers.food_serializers import FoodSerializer
 
 
 class FoodList(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, format=None):
         foods = Food.objects.filter(
